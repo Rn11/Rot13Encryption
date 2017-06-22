6f32fb887ba32df8fa9ce85986c4520dceffe524
Rn11 <max.lukas-nuelle@stud.hshl.de>
2017-06-22 18:40:40 +0200

    Added decryption

--- Rot13Encryption/Form1.cs
+++ Rot13Encryption/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Rot13Encryption
 {
@@ -41,7 +42,7 @@ namespace Rot13Encryption
                 // check if current char is between 65-122 (= a letter)
                 if (c >= 65 && c <= 122)
                 {
-                        input[i] = c = (char)('a' + (c - 'a' + 13) % 26);
+                    input[i] = c = (char)('a' + (c - 'a' + 13) % 26);
                 }
                 else
                 {
@@ -53,7 +54,24 @@ namespace Rot13Encryption
 
         private void btnDecrypt_Click(object sender, EventArgs e)
         {
-            getInput();
+            ArrayList input = getInput();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                // convert current letter to char
+                char c = Convert.ToChar(input[i]);
+                // decrement char by 13
+                // check if current char is between 65-122 (= a letter)
+                if (c >= 65 && c <= 122)
+                {
+                    input[i] = c = (char)('a' + (c - 'a' + 13) % 26);
+                }
+                else
+                {
+                    input[i] = c;
+                }
+            }
+            showResult(input);
         }
 
         private void showResult(ArrayList ar)
@@ -70,6 +88,11 @@ namespace Rot13Encryption
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ADJSGIHADSJGHADHSG");
         }
     }
 }
